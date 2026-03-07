366a4ba31733bf07d7f323039d3e60c20da7b5ac
manishh404 <97162140+manishh404@users.noreply.github.com>
2026-03-07 10:13:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Methanol KPI Digital Twin")
+
+st.write(
+"""
+Welcome to the KPI Monitoring Dashboard
+
+Use the sidebar to navigate:
+
+• Data Upload  
+• Tag Mapping Manager  
+• KPI Calculation  
+• KPI Dashboard
+"""
+)
---
+++ db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+
+DB_USER = "dtwinuser"
+DB_PASS = "bAnVojEFJSpYW9W0"
+DB_HOST = "192.168.10.189"
+DB_PORT = "8503"
+DB_NAME = "poc-digital-twin"
+
+engine = create_engine(
+    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+schema = "Methanol_KPI_Gen"
